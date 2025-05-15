3122d51a0a08fc15620b54f193056fab21980745
Ricardo <134720995+SadRico@users.noreply.github.com>
2025-05-15 12:59:05 +0200

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-import gui.MainWindow;
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(MainWindow.class);
-    }
-}
